05e2ebccec7fc7238d99f4c3435b26493dfe6ceb
DefenestrateMe <hello@defenestrate.me>
2018-05-21 13:15:15 +0100

    Fixed the road door PolyEdge positions

--- js/src/Road.js
+++ js/src/Road.js
@@ -15,7 +15,6 @@ export default class Road{
         let right = ((this.end.x+1)*Tile.SIZE) + (this.worldLoc.x * City.width * Tile.SIZE);
         let bottom = ((this.end.y+1)*Tile.SIZE) + (this.worldLoc.y * City.height * Tile.SIZE);
 
-
         // let's sort the entrances into the correct order
         let doors = {north:[],south:[],east:[],west:[]};
         let i;
@@ -50,32 +49,28 @@ export default class Road{
         this.poly.addNode(left, top);
 
         for (i = 0; i < doors.north.length; i++) {
-            // console.log('doing a north foor');
-            this.poly.addNode((doors.north[i].x*Tile.SIZE)+City.transX+8, top);
-            this.poly.addNode((doors.north[i].x*Tile.SIZE)+8+City.transX+8, top);
+            this.poly.addNode((this.worldLoc.x*City.width*Tile.SIZE)+(doors.north[i].x*Tile.SIZE)+8, top);
+            this.poly.addNode((this.worldLoc.x*City.width*Tile.SIZE)+(doors.north[i].x*Tile.SIZE)+8+8, top);
         }
 
         this.poly.addNode(right, top);
 
         for (i = 0; i < doors.east.length; i++) {
-            this.poly.addNode(right, (doors.east[i].y*Tile.SIZE)+City.transY+8);
-            this.poly.addNode(right, (doors.east[i].y*Tile.SIZE)+8+City.transY+8);
+            this.poly.addNode(right, (this.worldLoc.y*City.height*Tile.SIZE)+(doors.east[i].y*Tile.SIZE)+8);
+            this.poly.addNode(right, (this.worldLoc.y*City.height*Tile.SIZE)+(doors.east[i].y*Tile.SIZE)+8+8);
         }
 
         this.poly.addNode(right, bottom);
 
         for (i = 0; i < doors.south.length; i++) {
-            this.poly.addNode((doors.south[i].x*Tile.SIZE)+8+City.transX+8, bottom);
-            this.poly.addNode((doors.south[i].x*Tile.SIZE)+City.transX+8, bottom);
+            this.poly.addNode((this.worldLoc.x*City.width*Tile.SIZE)+(doors.south[i].x*Tile.SIZE)+8+8, bottom);
+            this.poly.addNode((this.worldLoc.x*City.width*Tile.SIZE)+(doors.south[i].x*Tile.SIZE)+8, bottom);
         }
-
-
-
 
         this.poly.addNode(left, bottom);
         for (i = 0; i < doors.west.length; i++) {
-            this.poly.addNode(left, (doors.west[i].y*Tile.SIZE)+8+City.transY+8);
-            this.poly.addNode(left, (doors.west[i].y*Tile.SIZE)+City.transY+8);
+            this.poly.addNode(left, (this.worldLoc.y*City.height*Tile.SIZE)+(doors.west[i].y*Tile.SIZE)+8+8);
+            this.poly.addNode(left, (this.worldLoc.y*City.height*Tile.SIZE)+(doors.west[i].y*Tile.SIZE)+8);
         }
         City.polyPath.addPoly(this.poly);
         this.poly.process();
